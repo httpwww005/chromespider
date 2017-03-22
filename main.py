30ece8463dd9384a30b4be4d96c0607a590350c1
httpwww005 <http.www.005@gmail.com>
2017-03-22 08:48:12 +0800

    updated

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bottle import route, run
 import os
 
-@route('/hello')
+@route('/')
 def hello():
     return "Hello World!"
 
 
-port = os.environ['PORT']
+port = int(os.environ.get('PORT',5000))
 run(host='0.0.0.0', port=port, debug=True)
