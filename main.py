f3d0ad46433c1ba9994324eea9e248ff244305cf
httpwww005 <http.www.005@gmail.com>
2017-03-22 21:06:34 +0800

    use CrawlerRunner and threading

--- main.py
+++ main.py
@@ -1,39 +1,79 @@
-from bottle import route, run
+import threading
+
+from bottle import get, route, run, template
+from bottle.ext.websocket import GeventWebSocketServer
+from bottle.ext.websocket import websocket
+
 import tablib
+import csv
 import os
 
 import scrapy
+from scrapy import signals
+from scrapy.crawler import Crawler
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from pydispatch import dispatcher
+from twisted.internet import reactor
+from billiard import Process
+from khcc.spiders.visitcount import VisitcountSpider
+from scrapy.utils.log import configure_logging
+from scrapy.crawler import CrawlerRunner
 
-dataset = tablib.Dataset()
+home = os.environ.get("HOME","/tmp")                 
+csv_file = os.path.join(home, "visitcount.csv") 
 
-@route('/run')
-def go():
-    import os
-    home = os.environ.get("HOME","/tmp")                 
-    csv = os.path.join(home, "visitcount.csv") 
 
-    try:
-        os.remove(csv)
-    except OSError:
-	pass
+configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
 
-    process = CrawlerProcess(get_project_settings())
-    process.crawl('visitcount')
-
-    process.start(stop_after_crawl=False) # the script will block here until the crawling is finished
-    
-    # process csv
-
-    with open(csv) as f:
-	dataset.csv = f.read()
+def get_csvtable():
+    dataset = tablib.Dataset()
+    with open(csv_file) as f:
+    	dataset.csv = f.read()
 
     return dataset.html
+
+
+
+@get('/')
+def index():
+    return template('index')
+
+@get('/websocket', apply=[websocket])
+def echo(ws):
+    while True:
+        msg = ws.receive()
+        msg = get_csvtable()
+        if msg is not None:
+            ws.send(msg)
+        else: 
+	    break
+
+
 
 @route('/view')
 def view():
-    return dataset.html
+    return get_csvtable()
+
+
+def run_spider(e):
+    try:
+        os.remove(csv_file)
+    except OSError:
+        pass
+
+    runner = CrawlerRunner(get_project_settings())
+    d = runner.crawl(VisitcountSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
+
+
+@route('/go')
+def go():
+    e = threading.Event()
+    t = threading.Thread(target=run_spider, args=(e,))
+    t.start()
+    return "processing"
 
 port = int(os.environ.get('PORT',5000))
-run(host='0.0.0.0', port=port, debug=True)
+run(host='0.0.0.0', port=port, debug=True, server=GeventWebSocketServer)
