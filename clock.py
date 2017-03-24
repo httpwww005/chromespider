6b10835b636d4cfd1c4057fa82fb08d47be80076
httpwww005 <http.www.005@gmail.com>
2017-03-24 12:57:38 +0800

    updated clock.py

--- clock.py
+++ clock.py
@@ -2,11 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', hour=12, minute=59)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
 
