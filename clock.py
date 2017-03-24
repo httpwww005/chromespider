eaf532b3175638a8a3db23b9af00c896968e0fbe
httpwww005 <http.www.005@gmail.com>
2017-03-24 13:07:35 +0800

    updated clock.py

--- clock.py
+++ clock.py
@@ -1,9 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import subprocess
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=13, minute=2)
+@sched.scheduled_job('cron', hour=13, minute=11)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    cmd = "scrapy crawl visitcount"
+    print('Running: %s' % cmd)
+    subprocess.Popen(cmd, shell=True)
 
 sched.start()
