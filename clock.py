2764083a8c41a847e3fa121941d2bf882d927b33
httpwww005 <http.www.005@gmail.com>
2017-03-27 10:20:58 +0800

    running?

--- clock.py
+++ clock.py
@@ -4,7 +4,8 @@ import random
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=random.randint(2,6), minute=random.randint(0,60))
+#@sched.scheduled_job('cron', hour=random.randint(2,6), minute=random.randint(0,60))
+@sched.scheduled_job('cron', hour=10, minute=23)
 def scheduled_job():
     cmd = "scrapy crawl visitcount"
     print('Late night crawler is running: %s' % cmd)
