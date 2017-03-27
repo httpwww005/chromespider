ef4e5cd6804793380b3bbeca23bf4f1e68a52f9c
httpwww005 <http.www.005@gmail.com>
2017-03-27 18:04:34 +0800

    updated

--- clock.py
+++ clock.py
@@ -18,28 +18,35 @@ job_id="scrapy_job"
 #@sched.scheduled_job('cron', id="scrapy_job", hour=3, minute=55)
 def scheduled_job():
     cmd = "scrapy crawl visitcount"
-    cmd = "ls"
     print('Late night crawler is running: %s' % cmd)
     subprocess.Popen(cmd, shell=True)
-    #sleep(check_period_hr*60*60 - 5*60)
+
+def get_next_run_time(is_refresh_run):
+    now = datetime.now()
+    hour=random.randint(18,20)
+    minute=random.randint(0,59)
+
+    if( is_refresh_run ):
+        year = now.year
+        month = now.month
+        day = now.day
+        next_run_time_ = datetime(year,month,day,hour,minute)
+    else:
+        next_run_time_ = next_run_time + timedelta(days=1)
+        next_run_time_ = next_run_time_.replace(hour=hour,minute=minute)
+
+    return next_run_time_
 
 sched.start()
+
+next_run_time = get_next_run_time()
 
 while True:
     jobs=sched.get_jobs()
 
     if( len(jobs) < 1 ):
-        now = datetime.now()
-        year = now.year
-        month = now.month
-        day = now.day
-        hour=random.randint(18,20)
-        minute=random.randint(0,59)
-        
-        next_run_time = datetime(year,month,day,hour,minute)
-            
-        job = sched.add_job(scheduled_job,id="scrapy_job",next_run_time=next_run_time)
-        #scheduler.reschedule_job('scrapy_job', trigger='cron', hour=hour, minute=minute)
+        next_run_time = get_next_run_time()
+        job = sched.add_job(scheduled_job,id="scrapy_job",next_run_time=get_next_run_time())
         print "new job scheduled at time: %s" % job.next_run_time
     
     total_sleep = timedelta(seconds=(check_period_hr*60*60 - scrapy_time*60))
