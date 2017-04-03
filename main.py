cbd77aa1dab706bfe235da4f79475384861d75be
httpwww005 <http.www.005@gmail.com>
2017-04-03 08:23:15 +0800

    tz_aware in db connection

--- main.py
+++ main.py
@@ -2,51 +2,54 @@ from __future__ import print_function
 import json
 import sys
 from bottle import get, route, run, template
-import os
 import datetime
 from datetime import date
-import pytz
 from bottle import response
 
 import logging
 logger = logging.getLogger()
 
-heroku_release = os.environ.get("HEROKU_RELEASE_VERSION","unknow")
-
+import os
 import pymongo
 import gridfs
+import pytz
+from bson.codec_options import CodecOptions
+
+TZ=pytz.timezone("Asia/Taipei")
 
 MONGODB_URI=os.environ["MONGODB_URI"]
 MONGODBCSV_URI=os.environ["MONGODBCSV_URI"]
 client = pymongo.MongoClient(MONGODB_URI)
 client_csv = pymongo.MongoClient(MONGODBCSV_URI)
-db = client["khcc"]
 db_csv = client_csv["csv"]
 fs_db = gridfs.GridFS(db_csv)
-collection = db["visitcount"]
+collection = client["khcc"]["visitcount"].with_options(codec_options=CodecOptions(tz_aware=True,tzinfo=TZ))
+
+heroku_release = os.environ.get("HEROKU_RELEASE_VERSION","unknow")
 
 header = ["created_on", "location", "address", "count"]
 
-TZ=pytz.timezone("Asia/Taipei")
 
 def get_rows(from_date, to_date):
     data = collection.find({"created_on":{"$gte":from_date,"$lt":to_date}})
-    rows = [[str(x["created_on"].replace(tzinfo=pytz.utc).astimezone(TZ).date()), x["location"], x["address"], x["count"]] for x in data]
+    rows = [[str(x["created_on"].date()), x["location"], x["address"], x["count"]] for x in data]
     return rows
 
 
 @get('/')
 def index():
     all_dates = list(collection.find({},{"created_on":1}).distinct("created_on"))
-    all_dates = sorted([str(x.replace(tzinfo=pytz.utc).astimezone(TZ).date()) for x in all_dates])
+    all_dates = sorted([str(x.date()) for x in all_dates])
     return template('index',header=header,dates=all_dates,heroku_release=heroku_release,re_created_on=re_created_on)
 
 re_created_on = "\d{4}-\d{2}-\d{2}"
 
 @route('/table/<created_on:re:%s>' % re_created_on)
 def table(created_on):
-    from_date = datetime.datetime.strptime(created_on,'%Y-%m-%d').replace(tzinfo=TZ).astimezone(pytz.utc)
+    from_date = datetime.datetime.strptime(created_on,'%Y-%m-%d').replace(tzinfo=TZ)
+    #print(from_date, file=sys.stderr)
     to_date = from_date + datetime.timedelta(days=1)
+    #print(to_date, file=sys.stderr)
     rows = get_rows(from_date, to_date)
     json_data = {"data":rows}
 
